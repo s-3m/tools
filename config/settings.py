7994fd8d7b1be941d4e341ebb0eeb7883eb949b3
s-3m <s-3m@yandex.ru>
2024-07-08 14:11:55 +0300

    fix media url

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = 'media'
+MEDIA_URL = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
